fd006698899b80a86fd3e4582e6dfc4f38358fee
Pavel Khrustalyov <pavel_khrustalyov@mail.ru>
2020-01-18 18:59:35 +0300

    make it better

--- server.js
+++ server.js
@@ -41,13 +41,11 @@ app.use('/api/comments', commentRoutes);
 app.use(errorHandler);
 app.use(compression());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
