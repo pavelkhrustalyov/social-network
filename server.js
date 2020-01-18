5fe1dc221759c8de1672537a6d0413df8b1759ac
Pavel Khrustalyov <pavel_khrustalyov@mail.ru>
2020-01-18 18:40:47 +0300

    prepare-two

--- server.js
+++ server.js
@@ -41,9 +41,13 @@ app.use('/api/comments', commentRoutes);
 app.use(errorHandler);
 app.use(compression());
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
